351f95bcb6819afe098acbcf0f979bec6d01ad3c
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-02-24 21:28:22 -0500

    Added image slider with nav bar and arrows to navigate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-param-reassign */
 const ddShort = document.getElementById("short");
 const ddButtons = document.getElementById("ddButtons");
+const sliderLeft = document.getElementById("sliderLeft");
+const sliderRight = document.getElementById("sliderRight");
+const navigationBar = document.getElementById("nav");
+const imagesDiv = document.querySelectorAll("#imagesDiv > div");
 ddShort.parentNode.removeChild(ddButtons);
+let interval;
+
+function activateCircle(images) {
+  images.forEach((e, i) => {
+    const circle = document.getElementById(i);
+    if (!e.classList.contains("hidden")) {
+      circle.classList.add("active");
+    } else if (circle.classList.contains("active"))
+      circle.classList.remove("active");
+  });
+}
+
+function createNav(nav, images) {
+  const numberOfCircles = images.length;
+  for (let i = 0; i < numberOfCircles; i++) {
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.id = i;
+    nav.appendChild(circle);
+  }
+}
 
 function removeMenu(shortDiv, menu, closeButton) {
   if (shortDiv.contains(closeButton)) {
@@ -45,4 +71,86 @@ function showDDMenu(shortDiv, buttons) {
   });
 }
 
+function slideImg(images, isLeft) {
+  let found = 0;
+  images.forEach((e, i) => {
+    if (!e.classList.contains("hidden") && !found) {
+      let previousElement;
+      if (isLeft) {
+        if (i === 0) previousElement = images.item(images.length - 1);
+        else previousElement = images.item(i - 1);
+      } else {
+        // eslint-disable-next-line no-lonely-if
+        if (i === images.length - 1) previousElement = images.item(0);
+        else previousElement = images.item(i + 1);
+      }
+      previousElement.classList.remove("hidden");
+      e.classList.add("hidden");
+      found = 1;
+      activateCircle(images);
+      clearInterval(interval);
+      interval = setInterval(() => {
+        slideImg(imagesDiv, 0);
+      }, 5000);
+    }
+  });
+  found = 0;
+}
+
+function navigate(images) {
+  const navCircles = document.querySelectorAll(".circle");
+  let circleNum;
+  // eslint-disable-next-line no-undef
+  navCircles.forEach((e) => {
+    e.addEventListener("click", () => {
+      images.forEach((i) => {
+        if (!i.classList.contains("hidden")) {
+          i.classList.add("hidden");
+        }
+      });
+      circleNum = parseInt(e.id, 10);
+      // eslint-disable-next-line no-undef
+      images.forEach((i, j) => {
+        if (j === circleNum) {
+          i.classList.remove("hidden");
+        }
+      });
+      activateCircle(images);
+      clearInterval(interval);
+      interval = setInterval(() => {
+        slideImg(imagesDiv, 0);
+      }, 5000);
+    });
+  });
+}
+
+sliderLeft.addEventListener("click", () => {
+  slideImg(imagesDiv, 1);
+});
+
+window.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "ArrowLeft":
+      slideImg(imagesDiv, 1);
+      break;
+
+    case "ArrowRight":
+      slideImg(imagesDiv, 0);
+      break;
+
+    default:
+      break;
+  }
+});
+
+sliderRight.addEventListener("click", () => {
+  slideImg(imagesDiv, 0);
+});
+
 showDDMenu(ddShort, ddButtons);
+createNav(navigationBar, imagesDiv);
+navigate(imagesDiv);
+// eslint-disable-next-line no-unused-vars
+interval = setInterval(() => {
+  slideImg(imagesDiv, 0);
+}, 5000);
